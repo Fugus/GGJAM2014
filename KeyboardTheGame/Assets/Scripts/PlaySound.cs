acadce0ff15140e9a3b045771b94978a98dedb03
Clement <clement.linel@gmail.com>
2014-01-25 07:16:45 -0500

    Updated tiles
    Added PlaySound when walk on tile.
    Added RecordSound when walk on tile.

---
+++ KeyboardTheGame/Assets/Scripts/PlaySound.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySound : MonoBehaviour
+{
+    public PlayerControl.RecordableSounds soundToPlay;
+
+    public void OnApplyTriggerAction()
+    {
+        GameObject playerGameObject = GameObject.FindGameObjectWithTag("Player");
+        PlayerControl playerControlScript = playerGameObject.GetComponent<PlayerControl>();
+
+        playerControlScript.PlaySound(soundToPlay);
+    }
+}
---
+++ KeyboardTheGame/Assets/Scripts/QuickTimer.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System.Collections;
+
+public class QuickTimer
+{
+	private float endTime = 0.0f;
+	private bool isStarted = false;
+
+	public QuickTimer ()
+	{
+	}
+
+	public void Start(float duration)
+	{
+		endTime = Time.time + duration;
+		isStarted = true;
+	}
+	
+	public void Stop()
+	{
+		isStarted = false;
+	}
+	
+	public bool IsStarted()
+	{
+		return isStarted;
+	}
+
+    public float GetElapsedTime()
+    {
+        if (!isStarted)
+        {
+            Debug.LogError("Calling IsElapsed() on a stopped QuickTimer");
+            return 0.0f;
+        }
+        
+        return endTime - Time.time;
+    }
+
+	public bool IsElapsed()
+	{
+		if (!isStarted)
+		{
+			Debug.LogError("Calling IsElapsed() on a stopped QuickTimer");
+			return false;
+		}
+		return Time.time > endTime;
+	}
+
+}
+
