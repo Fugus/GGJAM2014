7eff1dfbd6282fd6d6294b65b20a068b9fa26389
Clement <clement.linel@gmail.com>
2014-01-25 19:07:18 -0500

    Added Fog of War

---
+++ KeyboardTheGame/Assets/Scripts/Tiles/FogOfWarScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FogOfWarScript : MonoBehaviour
+{
+    public Material materialToSubstitute;
+    Material oldMaterial;
+
+    public void Start()
+    {
+        oldMaterial = transform.parent.renderer.material;
+        transform.parent.renderer.material = materialToSubstitute;
+        particleSystem.Play();
+    }
+
+    public void OnDisable()
+    {
+        particleSystem.Stop();
+        transform.parent.renderer.material = oldMaterial;
+    }
+}
--- KeyboardTheGame/Assets/Scripts/Tiles/TileScript.cs
+++ KeyboardTheGame/Assets/Scripts/Tiles/TileScript.cs
@@ -6,17 +6,40 @@ public class TileScript : MonoBehaviour {
 	protected UIManager UIManager_;
 	public string metadata = "";
 
+    bool hasBeenVisited = false;
+    GameObject spawnedFOW;
+
 	protected bool hasAppliedTriggerAction = false;
 	public void OnEnterTile()
 	{
 		hasAppliedTriggerAction = true;
-	}
-	
-	// Use this for initialization
-	protected void Start () {
-		UIManager_ = GameObject.Find("/Managers").GetComponent<UIManager>();
-	}
-	
+        RemoveFOW();
+    }
+
+    public void OnExitTile()
+    {
+    }
+
+    protected void Start()
+    {
+        UIManager_ = GameObject.Find("/Managers").GetComponent<UIManager>();
+        AddFOW();
+    }
+
+	void AddFOW()
+    {
+        Object fowGameObject = Resources.Load("FogOfWar");
+        Quaternion newRotation = transform.rotation;
+        newRotation *=  Quaternion.AngleAxis(180, Vector3.right);
+        spawnedFOW = GameObject.Instantiate(fowGameObject, transform.position, newRotation) as GameObject;
+        spawnedFOW.transform.parent = transform;
+    }
+
+    void RemoveFOW()
+    {
+        GameObject.Destroy(spawnedFOW);
+    }
+
 	// Update is called once per frame
 	protected void Update () {
 		if(hasAppliedTriggerAction)
