ad760ec537f0deed373c636387c8ebd365288912
Jonathan Osborne <jepo@jepo.id.au>
2014-01-25 22:00:46 -0500

    blah

--- KeyboardTheGame/Assets/Scripts/UIManager.cs
+++ KeyboardTheGame/Assets/Scripts/UIManager.cs
@@ -3,10 +3,14 @@ using System.Collections;
 
 public class UIManager : MonoBehaviour {
 
+	private GUISkin mainGUISkin;
+
 	private const int MAIN_TEXT_MARGIN = 100;
 	private const int MAIN_TEXT_TOP = 30;
 	private const int MAIN_TEXT_HEIGHT = 200;
 	private Rect? mainTextRect;
+
+	private Rect? resetRect;
 
 	public string mainTextString = "";
 
@@ -20,9 +24,25 @@ public class UIManager : MonoBehaviour {
 	}
 
 	void OnGUI () {
+		if(mainGUISkin == null)
+			mainGUISkin = Resources.Load<GUISkin>("MainGUISkin");
+
 		if(mainTextRect == null)
 			mainTextRect = new Rect(MAIN_TEXT_MARGIN, MAIN_TEXT_TOP, Screen.width - MAIN_TEXT_MARGIN * 2, MAIN_TEXT_HEIGHT);
-		GUI.Label(mainTextRect.GetValueOrDefault(), mainTextString);
+		//GUI.Label(mainTextRect.GetValueOrDefault(), mainTextString, mainGUISkin.label);
+		GUI.Box (mainTextRect.GetValueOrDefault(), mainTextString, mainGUISkin.box);
+
+		if(resetRect == null)
+			resetRect = new Rect(0, 0, 100, 20);
+		if(GUI.Button(resetRect, "Reset")) {
+			ResetGame();
+		};
 	}
-	
+
+	public void ResetGame()
+	{
+		// Refresh LevelManager
+
+	}
+
 }
